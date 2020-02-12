6833889076c89708b0d479aa2176f28e8ba50db0
Francois van Heerden <36744115+fvanheer@users.noreply.github.com>
2020-02-12 14:21:15 +0200

    Add files via upload

--- google_query.py
+++ google_query.py
@@ -1,35 +1,3 @@
-
-# Query Google Analytics API
-
-Simple and useful approach to include some Google Analytics data in your ETL process or inrich your data for analytics with transactional data. This boalerplate example will enable you to customise your queries to your business needs.  
-
-* [Google API]() - More info
-* [Google2Pandas](https://github.com/panalysis/Google2Pandas) - More info
-* [Google Dimensions & Metrics Explorer](https://ga-dev-tools.appspot.com/dimensions-metrics-explorer/) - More info on dimensions and metrics that can be queried via the Google Analytics API
-
-## Built With
-
-* [Python 3.7](https://www.python.org/downloads/release/python-370/) - More information 
-
-### Quick setup
-
-```
-pip install -r requirements.txt
-```
-
-or pip install 
-```
-google2pandas
-pandas
-```
-### The Model
-
-You can find the code in the google_query.py file
-
-## Import the libraries
-
-```Python
-
 ##########################################################################################################################################
 ### GOOGLE ANALYTICS API QUERY TO PANDAS ###
 ##########################################################################################################################################
@@ -41,11 +9,7 @@ import pandas as pd
 #query parameters
 max_result = 10000
 store = 4363436
-```
 
-## Define the google analytics queries
-
-```Python
 ##########################################################################################################################################
 ### DEFINE QUERIES ###
 ##########################################################################################################################################
@@ -68,11 +32,8 @@ query_returning = {\
     'filters'       : 'userType==Returning Visitor',
     'start_date'    : '8daysAgo',
     'max_results'   : max_result}
-```
 
-## Setup User Configuration
 
-```Python
 ##########################################################################################################################################
 ### USER CONFIGURATION ###
 ##########################################################################################################################################
@@ -80,11 +41,7 @@ query_returning = {\
 conn = GoogleAnalyticsQuery(
         token_file_name='my_analytics.dat',
 	secrets='my_client_secrets_v3.json')
-```
 
-## Run the Query
-
-```Python
 ##########################################################################################################################################
 ### DEFINE FUNCTION TO QUERY AND APPEND DATA ###
 ##########################################################################################################################################
@@ -104,15 +61,3 @@ df_final = google_df()
 
 # write to csv
 df_final.to_csv('google_user_by_device.csv')
-```
-
-### Deployment Options
-* Include the computations as a part of your ETL process (I use [KNIME](https://www.knime.com/)) - Include it as a step before writing the final customer lifetime table to your Data Warehouse.
-
-## Author
-
-* **Francois van Heerden** - *Experience* - [LinkedIn Profile](https://www.linkedin.com/in/francois-van-heerden-9589825a/)
-
-## Acknowledgments
-
-* Found inspiration from multiple fellow Data Scientists in the open source 
